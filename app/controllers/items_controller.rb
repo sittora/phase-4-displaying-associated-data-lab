50fd516dd381f81a2469686764c35ad8154b69fd
sittora <sittora@gmail.com>
2021-10-20 19:33:13 -0400

    Assignment completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        items_json = items.to_json(except: [:created_at, :updated_at], include: [user: { except: [:created_at, :updated_at]}])
+        render json: items_json
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        user_json = user.to_json(except: [:created_at, :updated_at], include: [items: { except: [:created_at, :updated_at]}])
+        render json: user_json
+    end
 end
